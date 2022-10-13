e48974d5b7c562b75c09877acb9d381e9ada1eca
Александра Малышева <leksamal@icloud.com>
2022-10-13 14:03:23 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Captcha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.picBox = new System.Windows.Forms.PictureBox();
+            this.txtCaptcha = new System.Windows.Forms.TextBox();
+            this.txtPassw = new System.Windows.Forms.TextBox();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.button = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Image = global::Captcha.Properties.Resources.open;
+            this.button1.Location = new System.Drawing.Point(398, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(27, 25);
+            this.button1.TabIndex = 37;
+            this.button1.Text = " ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // picBox
+            // 
+            this.picBox.Location = new System.Drawing.Point(116, 273);
+            this.picBox.Name = "picBox";
+            this.picBox.Size = new System.Drawing.Size(204, 98);
+            this.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBox.TabIndex = 36;
+            this.picBox.TabStop = false;
+            // 
+            // txtCaptcha
+            // 
+            this.txtCaptcha.Location = new System.Drawing.Point(222, 407);
+            this.txtCaptcha.Name = "txtCaptcha";
+            this.txtCaptcha.Size = new System.Drawing.Size(159, 20);
+            this.txtCaptcha.TabIndex = 35;
+            // 
+            // txtPassw
+            // 
+            this.txtPassw.ImeMode = System.Windows.Forms.ImeMode.On;
+            this.txtPassw.Location = new System.Drawing.Point(188, 220);
+            this.txtPassw.Name = "txtPassw";
+            this.txtPassw.Size = new System.Drawing.Size(193, 20);
+            this.txtPassw.TabIndex = 34;
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Location = new System.Drawing.Point(188, 158);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(193, 20);
+            this.txtLogin.TabIndex = 33;
+            this.txtLogin.TextChanged += new System.EventHandler(this.txtLogin_TextChanged);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(188, 125);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(193, 20);
+            this.txtName.TabIndex = 32;
+            // 
+            // button
+            // 
+            this.button.BackColor = System.Drawing.Color.Honeydew;
+            this.button.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button.Location = new System.Drawing.Point(148, 453);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(119, 28);
+            this.button.TabIndex = 31;
+            this.button.Text = "Регистрация";
+            this.button.UseVisualStyleBackColor = false;
+            this.button.Click += new System.EventHandler(this.button_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Aquamarine;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(13, 407);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 16);
+            this.label5.TabIndex = 30;
+            this.label5.Text = "Введите текст с картинки";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Aquamarine;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(83, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 16);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Пароль";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Aquamarine;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(94, 159);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 28;
+            this.label3.Text = "Логин\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Aquamarine;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 16);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Имя пользователя\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Aquamarine;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(48, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 16);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Придумайте и введите данные для регистрации";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(186, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(214, 24);
+            this.label6.TabIndex = 38;
+            this.label6.Text = "*разрешается вводить только английские буквы\r\n*логин должен быть длинее 4х символ" +
+    "ов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Ivory;
+            this.ClientSize = new System.Drawing.Size(431, 529);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picBox);
+            this.Controls.Add(this.txtCaptcha);
+            this.Controls.Add(this.txtPassw);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox picBox;
+        private System.Windows.Forms.TextBox txtCaptcha;
+        private System.Windows.Forms.TextBox txtPassw;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button button;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using Captcha.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Captcha
+{
+    public partial class Form1 : Form
+    {
+        public int id;
+        public FileInfo fileInfo;
+        public Random rnd;
+
+        public Form1()
+        {
+            InitializeComponent();
+            rnd = new Random();
+            id = rnd.Next(0, pathFiles.Length);
+            picBox.Image = new Bitmap(pathFiles[id]);
+            fileInfo = new FileInfo(pathFiles[id]);
+
+
+        }
+
+        string[] pathFiles = Directory.GetFiles("../../Res/", "*.png", SearchOption.AllDirectories);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtPassw.UseSystemPasswordChar = true;
+        }
+
+
+        private void txtLogin_TextChanged(object sender, EventArgs e)
+        {
+            txtLogin.Text = System.Text.RegularExpressions.Regex.Replace
+                            (txtLogin.Text, @"[^a-zA-Z]", "");
+        }
+
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "" &&
+                txtPassw.Text != "" &&
+                txtLogin.Text != "" &&
+                txtLogin.TextLength >= 4 &&
+                txtCaptcha.Text == Convert.ToString(fileInfo.Name.Replace(".png", "")))
+            {
+                id = rnd.Next(0, pathFiles.Length);
+                picBox.Image = new Bitmap(pathFiles[id]);
+                fileInfo = new FileInfo(pathFiles[id]);
+                txtCaptcha.Text = "";
+                txtLogin.Text = "";
+                txtName.Text = "";
+                txtPassw.Text = "";
+
+                MessageBox.Show(
+            "Спасибо за регистрацию на сайте ☺",
+            "Сообщение",
+            MessageBoxButtons.OK,
+            MessageBoxIcon.Information,
+            MessageBoxDefaultButton.Button1,
+            MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else
+            if (txtLogin.TextLength <= 4)
+            {
+                MessageBox.Show(
+                           "Логин должен содержать более 4х символов",
+                           "!!!",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Error,
+                           MessageBoxDefaultButton.Button1,
+                           MessageBoxOptions.DefaultDesktopOnly);
+            }
+            else
+            {
+                MessageBox.Show(
+                           "Заполните все поля",
+                           "!!!",
+                           MessageBoxButtons.OK,
+                           MessageBoxIcon.Error,
+                           MessageBoxDefaultButton.Button1,
+                           MessageBoxOptions.DefaultDesktopOnly);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtPassw.PasswordChar == '●')
+            {
+                txtPassw.UseSystemPasswordChar = false;
+                button1.Image = Image.FromFile("C:/Users/leksa/OneDrive/Документы/3 КУРС/МДК 02.01 Силахина/Captcha/Resources/close.png");
+            }
+            else
+            {
+                button1.Image = Image.FromFile("C:/Users/leksa/OneDrive/Документы/3 КУРС/МДК 02.01 Силахина/Captcha/Resources/open.png");
+                txtPassw.UseSystemPasswordChar = true;
+            }
+
+        }
+
+
+    }
+}
